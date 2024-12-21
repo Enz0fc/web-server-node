28205e5e97824e1f10e47f2bb71299992a95dc74
Enz0fc <enzofc2003@fpuna.edu.py>
2024-12-21 13:43:39 -0300

    primer commit

---
+++ src/server/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express')
+const path = require('path')
+
+const startServer = (option) => {
+    const {port, public_path = 'public'} = option
+    console.log(port)
+    console.log(public_path)
+
+    const app = express()
+    //para poder usar middlewares se usa la palabra use, propio de express
+    app.use(express.static(public_path)) //contenido estatico disponible para usar
+
+    app.get('*',(req,res)=>{
+        const indexPath = path.join(`../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+    app.listen(port, () =>{
+        console.log(`Escuchando en el pureto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
